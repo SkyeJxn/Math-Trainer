486e87d19cb3559e8e7031dd7e7db73a154256bf
SkyeJxn <78538146+SkyeJxn@users.noreply.github.com>
2025-06-06 19:00:01 +0200

    Add Calculatortest

---
+++ CalculatorTest.java
@@ -0,0 +1,22 @@
+public class CalculatorTest {
+    public static void main(String[] args) {
+        Calculator test = new Calculator();
+        int range = 10;
+        int in = 3;
+
+        for (int i = 0; i < 10; i++){
+            String str = test.newTask(range);
+            System.out.println(str);
+            if(i%2 == 0){
+                in = test.getRes();
+            }
+            System.out.println("Simulierte Eingabe: " + in);
+            if (test.checkresult(in)){
+                System.out.println("Correct");
+            }
+            else{
+                System.out.println("Wrong");
+            }
+        }
+    }
+}
