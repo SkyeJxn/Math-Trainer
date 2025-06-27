e1a2771e7ad355a1a36cd7c82ac68f5c8f6d93dd
SkyeJxn <78538146+SkyeJxn@users.noreply.github.com>
2025-06-27 21:29:02 +0200

    feat: running websocket server

--- src/main/java/websocket/WebsocketConnector.java
+++ src/main/java/websocket/WebsocketConnector.java
@@ -6,10 +6,12 @@ import java.util.concurrent.ConcurrentHashMap;
 
 import core.GameController;
 import core.GamePrints;
+import jakarta.websocket.OnClose;
 import jakarta.websocket.OnMessage;
 import jakarta.websocket.OnOpen;
 import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
+@SuppressWarnings("ConvertToTryWithResources")
 
 @ServerEndpoint("/math")
 public class WebsocketConnector {
@@ -19,12 +21,14 @@ public class WebsocketConnector {
         int configStep = 0;
         boolean useLives, useTaskTimer, useRoundTimer = false;
         int tasks = 5, range = 10, lives = 3;
+        int currentTask = 0;
         GameController game;
         boolean gameLoop = false;
     }
 
     @OnOpen
     public void onOpen(Session session) throws IOException{
+        System.out.println("started new session. ID: " + session.getId());
         runState state = new runState();
         sess.put(session, state);
         session.getBasicRemote().sendText(GamePrints.printBox(" MATH TRAINER "));
@@ -36,36 +40,54 @@ public class WebsocketConnector {
         runState run = sess.get(session);
         if (run == null){
             session.getBasicRemote().sendText("Session Error. reconnect");
+            session.close();
         }
 
         if (!run.gameLoop){
             config(session, run, message);
         } else {
+            if (run.game == null) startGame(session, run);
             try{
                 int ans = Integer.parseInt(message);
                 String res = run.game.checkTask(ans);
                 session.getBasicRemote().sendText(res);
 
+                run.currentTask++;
+
                 if(!run.game.getLiving()){
-                    session.getBasicRemote().sendText("Game Over!\n" + run.game.gameEnding());
+                    session.getBasicRemote().sendText(String.format("Game Over!\n%s\n%s\nClosing connection now.", run.game.endRoundTime(), run.game.gameEnding()));
                     sess.remove(session);
-                } else {
-                    session.getBasicRemote().sendtext(run.game.newTask());
+                    session.close();
+                } else if(run.currentTask >= run.tasks){
+                    session.getBasicRemote().sendText(String.format("Round finished!\n%s\n%s\nClosing connection now.", run.game.endRoundTime(), run.game.gameEnding()));
+                    sess.remove(session);
+                    session.close();
+                } else{
+                    session.getBasicRemote().sendText(run.game.newTask());
                 }
+            } catch (NumberFormatException e){
+                session.getBasicRemote().sendText("Enter valid number");
             }
         }
     }
 
+    @OnClose
+    public void onClose(Session session){
+        System.out.println("Session closed. ID: " + session.getId());
+    }
+
     private void config(Session session, runState run, String message) throws IOException{
         switch(run.configStep){
-            case 0:
+            case 0 -> {
                 run.gameLoop = message.trim().equalsIgnoreCase("n");
                 if (!run.gameLoop){
                     run.configStep++;
                     session.getBasicRemote().sendText("Set new Task range:");
+                } else {
+                    startGame(session, run);
                 }
-                break;
-            case 1:
+            }
+            case 1 -> {
                 try {
                     run.range = Integer.parseInt(message.trim());
                 } catch (NumberFormatException e) {
@@ -73,8 +95,8 @@ public class WebsocketConnector {
                 }
                 run.configStep++;
                 session.getBasicRemote().sendText("Set new Task count:");
-                break;
-            case 2:
+            }
+            case 2 -> {
                 try {
                     run.tasks = Integer.parseInt(message.trim());
                 } catch (NumberFormatException e) {
@@ -82,23 +104,23 @@ public class WebsocketConnector {
                 }
                 run.configStep++;
                 session.getBasicRemote().sendText("Use Task Timer?: (Y/N)");
-                break;
-            case 3:
+            }
+            case 3 -> {
                 run.useTaskTimer = message.trim().equalsIgnoreCase("Y");
                 run.configStep++;
                 session.getBasicRemote().sendText("Use Round Timer?: (Y/N)");
-                break;
-            case 4:
+            }
+            case 4 -> {
                 run.useRoundTimer = message.trim().equalsIgnoreCase("Y");
                 run.configStep++;
                 session.getBasicRemote().sendText("Use Lives?: (Y/N)");
-                break;
-            case 5:
+            }
+            case 5 -> {
                 run.useLives = message.trim().equalsIgnoreCase("Y");
                 run.configStep++;
                 if (run.useLives) session.getBasicRemote().sendText("How many Lives?:");
-                break;
-            case 6:
+            }
+            case 6 -> {
                 if (run.useLives){
                     try {
                         run.lives = Integer.parseInt(message);
@@ -106,12 +128,16 @@ public class WebsocketConnector {
                         run.lives = 3;
                     }
                 }
+                run.gameLoop = true;
+
                 startGame(session, run);
+            }
         }
     }
 
     private void startGame(Session session, runState run) throws IOException{
         run.game = new GameController();
+        run.currentTask = 0;
         run.game.changeConfig(
             run.useLives,
             run.useTaskTimer,
@@ -120,7 +146,7 @@ public class WebsocketConnector {
             run.range,
             run.lives
         );
-        run.gameLoop = true;
         session.getBasicRemote().sendText(run.game.newTask());
+        run.game.startRoundTime();
     }
 }
