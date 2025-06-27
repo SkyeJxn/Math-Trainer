6660c59dc125b50283833dd4dc1df4a7201cf707
SkyeJxn <78538146+SkyeJxn@users.noreply.github.com>
2025-06-27 20:26:30 +0200

    theoretically functional socket

--- src/main/java/websocket/WebsocketConnector.java
+++ src/main/java/websocket/WebsocketConnector.java
@@ -1,15 +1,126 @@
 package websocket;
 
-import javax.websocket.OnMessage;
-import javax.websocket.Session;
+import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
+import core.GameController;
+import core.GamePrints;
+import jakarta.websocket.OnMessage;
+import jakarta.websocket.OnOpen;
+import jakarta.websocket.Session;
 import jakarta.websocket.server.ServerEndpoint;
 
 @ServerEndpoint("/math")
 public class WebsocketConnector {
-    
+    private static final Map<Session, runState> sess = new ConcurrentHashMap<>();
+
+    private static class runState{
+        int configStep = 0;
+        boolean useLives, useTaskTimer, useRoundTimer = false;
+        int tasks = 5, range = 10, lives = 3;
+        GameController game;
+        boolean gameLoop = false;
+    }
+
+    @OnOpen
+    public void onOpen(Session session) throws IOException{
+        runState state = new runState();
+        sess.put(session, state);
+        session.getBasicRemote().sendText(GamePrints.printBox(" MATH TRAINER "));
+        session.getBasicRemote().sendText("Want to Configure the game? (Y/N)");
+    }
+
     @OnMessage
-    public String onMessage(String message, Session session){
-        return "Ret: " + message;
+    public void onMessage(String message, Session session) throws IOException{
+        runState run = sess.get(session);
+        if (run == null){
+            session.getBasicRemote().sendText("Session Error. reconnect");
+        }
+
+        if (!run.gameLoop){
+            config(session, run, message);
+        } else {
+            try{
+                int ans = Integer.parseInt(message);
+                String res = run.game.checkTask(ans);
+                session.getBasicRemote().sendText(res);
+
+                if(!run.game.getLiving()){
+                    session.getBasicRemote().sendText("Game Over!\n" + run.game.gameEnding());
+                    sess.remove(session);
+                } else {
+                    session.getBasicRemote().sendtext(run.game.newTask());
+                }
+            }
+        }
+    }
+
+    private void config(Session session, runState run, String message) throws IOException{
+        switch(run.configStep){
+            case 0:
+                run.gameLoop = message.trim().equalsIgnoreCase("n");
+                if (!run.gameLoop){
+                    run.configStep++;
+                    session.getBasicRemote().sendText("Set new Task range:");
+                }
+                break;
+            case 1:
+                try {
+                    run.range = Integer.parseInt(message.trim());
+                } catch (NumberFormatException e) {
+                    run.range = 10;
+                }
+                run.configStep++;
+                session.getBasicRemote().sendText("Set new Task count:");
+                break;
+            case 2:
+                try {
+                    run.tasks = Integer.parseInt(message.trim());
+                } catch (NumberFormatException e) {
+                    run.tasks = 5;
+                }
+                run.configStep++;
+                session.getBasicRemote().sendText("Use Task Timer?: (Y/N)");
+                break;
+            case 3:
+                run.useTaskTimer = message.trim().equalsIgnoreCase("Y");
+                run.configStep++;
+                session.getBasicRemote().sendText("Use Round Timer?: (Y/N)");
+                break;
+            case 4:
+                run.useRoundTimer = message.trim().equalsIgnoreCase("Y");
+                run.configStep++;
+                session.getBasicRemote().sendText("Use Lives?: (Y/N)");
+                break;
+            case 5:
+                run.useLives = message.trim().equalsIgnoreCase("Y");
+                run.configStep++;
+                if (run.useLives) session.getBasicRemote().sendText("How many Lives?:");
+                break;
+            case 6:
+                if (run.useLives){
+                    try {
+                        run.lives = Integer.parseInt(message);
+                    } catch (NumberFormatException e) {
+                        run.lives = 3;
+                    }
+                }
+                startGame(session, run);
+        }
+    }
+
+    private void startGame(Session session, runState run) throws IOException{
+        run.game = new GameController();
+        run.game.changeConfig(
+            run.useLives,
+            run.useTaskTimer,
+            run.useRoundTimer,
+            run.tasks,
+            run.range,
+            run.lives
+        );
+        run.gameLoop = true;
+        session.getBasicRemote().sendText(run.game.newTask());
     }
 }
--- src/main/java/websocket/WebsocketMain.java
+++ src/main/java/websocket/WebsocketMain.java
@@ -1,18 +1,27 @@
 package websocket;
 
+import java.io.IOException;
+
 import org.glassfish.tyrus.server.Server;
+
+import jakarta.websocket.DeploymentException;
 
 public class WebsocketMain {
 
     public static void main(String[] args) {
-        Server sock = new Server("Localhost", 8080, "/ws", WebsocketConnector.class);
+
+        String host = "localhost";
+        int port = 8080;
+        String path = "/ws";
+        String link = String.format("ws://%s:%d%s/math", host, port, path);
+        Server sock = new Server(host, port, path,null, WebsocketConnector.class);
 
         try {
             sock.start();
-            System.out.println("Server started. link: ws://localhost:8080/ws/math");
+            System.out.println("Server started. link: " + link);
             System.in.read();
-        } catch (java.io.IOException | javax.websocket.DeploymentException e) {
-            e.printStackTrace();
+        } catch (DeploymentException | IOException e) {
+            System.out.println(e);
         } finally {
             sock.stop();
         }
